afaba16cbf825808a5dbaa5834c198e847eeb031
abhishekdhapare9392 <abhishek.dhapare@gmail.com>
2021-11-16 19:21:19 +0530

    Google Insight API integrated

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const pa11y = require('pa11y')
+const fetch = require('node-fetch')
+dotenv.config()
+
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.use(express.static('public/public'))
 
-app.get('/api/test', async (req, res) => {
+app.get('/api/issues', async (req, res) => {
   if (!req.query.url) {
     res.status(400).json({
       error: 'Required URL',
@@ -20,6 +24,20 @@ app.get('/api/test', async (req, res) => {
         error: 'Invalid URL!',
       })
     }
+  }
+})
+
+app.get('/api/insight', async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({
+      error: 'Required URL',
+    })
+  } else {
+    fetch(
+      `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${req.query.url}&key=${process.env.API_KEY}&category=SEO&category=PERFORMANCE&strategy=DESKTOP`,
+    )
+      .then((response) => response.json())
+      .then((data) => res.status(200).json(data))
   }
 })
 
--- public/src/App.svelte
+++ public/src/App.svelte
@@ -3,21 +3,28 @@
 	let loader = false;
 	let onload = true;
 	let issues = [];
+	let insights = [];
 	let errorText = '';
 	const submitUrl = async (e)=>{
 		loader = true
 		if(url){
-			let response = await fetch(`/api/test?url=${url}`)
-			if (response.status === 200) {
+			let response = await fetch(`/api/issues?url=${url}`)
+			let seoResponse = await fetch(`/api/insight?url=${url}`)
+			if (response.status === 200 && seoResponse.status === 200) {
 				const data = await response.json()
+				const seoData = await seoResponse.json()
 				issues = data.issues;
+				insights = seoData.lighthouseResult.audits;
+				console.log(insights.length)
 				if (issues.length > 0) {
 					loader = false
 					errorText = "Congrats!!! No issue found."
 				}
 				onload = false
+
 			} else{
 				issues = []
+				insights =[]
 				loader = false
 				onload = false
 				const err = await response.json();
@@ -25,6 +32,7 @@
 			}
 		} else {
 			issues = []
+			insights = []
 			loader = false
 			onload = false
 			errorText = "URL Required!";
@@ -41,6 +49,7 @@
 	<!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </svelte:head>
 
 <section class="section">
@@ -59,6 +68,8 @@
 				</div>
 			</div>
 		</div>
+
+		
 		{#if loader === true}
 		<div class="d-flex flex-row justify-content-center align-center">
 			<div class="spinner-border" role="status">
@@ -69,19 +80,59 @@
 		{#if loader === false && issues.length > 0}
 		<div class="row py-5">
 			<div class="col-12 col-md-12 col-sm-12">
-				{#each issues as issue}
-				<div class="card mb-3 shadow-sm">
-					<div class="card-body">
-						<h4>{issue.message}</h4>
-						<p class="bg-light p-3 my-3">
-							{issue.context}
-						</p>
-						<p class="bg-primary text-light p-2">
-							CODE: {issue.code}
-						</p>
+				<ul class="nav nav-tabs" id="myTab" role="tablist">
+					<li class="nav-item" role="presentation">
+					  <button class="nav-link active" id="access-tab" data-bs-toggle="tab" data-bs-target="#access" type="button" role="tab" aria-controls="access" aria-selected="true">Accessibility</button>
+					</li>
+					<li class="nav-item" role="presentation">
+					  <button class="nav-link" id="seo-tab" data-bs-toggle="tab" data-bs-target="#seo" type="button" role="tab" aria-controls="seo" aria-selected="false">SEO</button>
+					</li>
+				  </ul>
+				  <div class="tab-content" id="myTabContent">
+					<div class="tab-pane fade show active" id="access" role="tabpanel" aria-labelledby="access-tab">
+						{#each insights as insight, key}
+							{insight[key]}
+							{#if insight.score == 0 || insight.score == null}
+								<div class="card mb-3 shadow-sm">
+									<div class="card-body">
+										<h4>{insight.title}</h4>
+										<p class="bg-light p-3 my-3">
+											{insight.description.split('. ')[0]}
+										</p>
+									</div>
+								</div>
+							{/if}
+						{/each}
+						{#each issues as issue}
+						<div class="card mb-3 shadow-sm">
+							<div class="card-body">
+								<h4>{issue.message}</h4>
+								<p class="bg-light p-3 my-3">
+									{issue.context}
+								</p>
+								<p class="bg-primary text-light p-2">
+									CODE: {issue.code}
+								</p>
+							</div>
+						</div>
+						{/each}
 					</div>
-				</div>
-				{/each}
+					<div class="tab-pane fade" id="seo" role="tabpanel" aria-labelledby="seo-tab">
+						{#each insights as insight, key}
+							{{key}}
+							{#if insight.score == 0 || insight.score == null}
+								<div class="card mb-3 shadow-sm">
+									<div class="card-body">
+										<h4>{insight.title}</h4>
+										<p class="bg-light p-3 my-3">
+											{insight.description.split('. ')[0]}
+										</p>
+									</div>
+								</div>
+							{/if}
+						{/each}
+					</div>
+				  </div>
 			</div>
 		</div>
 		{:else if loader === false && issues.length === 0 && onload === false}
